1a3b7532964cc78dadc27c0dd59131f30efb9255
mbingham <mark.bingham@interworks.com>
2024-10-04 16:08:30 -0500

    config update

--- config/tableau.php
+++ config/tableau.php
@@ -2,9 +2,16 @@
 
 // config for InterWorks/Tableau
 return [
-    'base_url' => env('TABLEAU_BASE_URL'),
-    'username' => env('TABLEAU_USERNAME'),
-    'password' => env('TABLEAU_PASSWORD'),
-    'site_url' => env('TABLEAU_SITE_URL'),
-    'version'  => env('TABLEAU_PRODUCT_VERSION', '2020.1'),
+    'base_url'    => env('TABLEAU_URL', 'https://your-tableau-server.com'),
+    'product'     => env('TABLEAU_PRODUCT_VERSION', '2021.1'),
+    'credentials' => [
+        // Username and password auth (not required if using PAT)
+        'username'   => env('TABLEAU_USERNAME', ''),
+        'password'   => env('TABLEAU_PASSWORD', ''),
+        // PAT auth (not required if using username and password)
+        'pat_name'   => env('TABLEAU_PAT_NAME', ''),
+        'pat_secret' => env('TABLEAU_PAT_SECRET', ''),
+        // Site name
+        'site_name'  => env('TABLEAU_SITE_NAME', '')
+    ],
 ];
