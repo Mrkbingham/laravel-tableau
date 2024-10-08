976f59b129a9dfae9095c15bad7a0dba4ee39b4c
mbingham <mark.bingham@interworks.com>
2024-10-08 11:04:02 -0500

    initial workbook add and test

--- tests/API/WorkbooksTest.php
+++ tests/API/WorkbooksTest.php
@@ -5,40 +5,74 @@ use InterWorks\Tableau\API\Workbooks;
 use InterWorks\Tableau\TableauAPI;
 
 beforeEach(function () {
-    $this->siteId = 'test-site-id';
-    $this->token  = 'test-token';
+    $this->siteId = Config::get('tableau.site_id');
 
     // Initialize the Workbooks class with a token
     $this->tableau = new TableauAPI();
 });
 
-describe('WorkbooksAPI', function () {
-    it('can fetch all workbooks', function () {
-        // Call the method to get all workbooks
-        $response = $this->tableau->workbooks()->getAllWorkbooks($this->siteId);
-
-        // Assert the response contains the workbooks array
-        expect($response)->toHaveKey('workbooks');
-        expect($response['workbooks'])->toHaveCount(2);
-    });
-
-    it('can fetch a single workbook by id', function () {
+describe('WorkbooksTest', function () {
+    it('can addTags', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can deleteTag', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can delete', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can download', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can downloadEncryptedKeychain', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can downloadPDF', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can downloadPowerPoint', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can downloadRevision', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can getDowngradeInfo', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can getRevisions', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can publish', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can queryViews', function () {
+        // Not implemented
+    })->skip('not yet implemented');
+    it('can getWorkbookById', function () {
         // Call the method to get a workbook by ID
-        $workbookId = 'workbook1';
-        $response = $this->tableau->workbooks()->getWorkbookById($this->siteId, $workbookId);
+        $workbookContentURL = env('TABLEAU_SERVER_WORKBOOK_CONTENT_URL');
+        $workbookID = env('TABLEAU_SERVER_WORKBOOK_ID');
+        $workbookName = env('TABLEAU_SERVER_WORKBOOK_NAME');
+        $response = $this->tableau->workbooks()->getWorkbookById($workbookID);
 
         // Assert the response contains the correct workbook data
         expect($response)->toHaveKey('workbook');
-        expect($response['workbook']['id'])->toBe('workbook1');
-        expect($response['workbook']['name'])->toBe('Workbook 1');
+        expect($response['workbook']['contentUrl'])->toBe($workbookContentURL);
+        expect($response['workbook']['id'])->toBe($workbookID);
+        expect($response['workbook']['name'])->toBe($workbookName);
     });
+    it('can getWorkbookByContentURL', function () {
+        // Call the method to get a workbook by ID
+        $workbookContentURL = env('TABLEAU_SERVER_WORKBOOK_CONTENT_URL');
+        $workbookID = env('TABLEAU_SERVER_WORKBOOK_ID');
+        $workbookName = env('TABLEAU_SERVER_WORKBOOK_NAME');
+        $response = $this->tableau->workbooks()->getWorkbookByContentURL($workbookContentURL);
+        dd($response);
 
-    it('can delete a workbook', function () {
-        // Call the method to delete a workbook
-        $workbookId = 'workbook1';
-        $response = $this->tableau->workbooks()->deleteWorkbook($this->siteId, $workbookId);
-
-        // Assert the response is successful
-        expect($response)->toBeTrue();
+        // Assert the response contains the correct workbook data
+        expect($response)->toHaveKey('workbook');
+        expect($response['workbook']['contentUrl'])->toBe($workbookContentURL);
+        expect($response['workbook']['id'])->toBe($workbookID);
+        expect($response['workbook']['name'])->toBe($workbookName);
     });
 });
