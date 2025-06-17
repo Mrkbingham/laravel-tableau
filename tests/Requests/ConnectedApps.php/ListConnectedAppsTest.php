6e0b2d2ba6d9227aa32839da62c3598e57bf3829
mbingham <mark.bingham@interworks.com>
2025-06-17 09:51:41 -0500

    updates tests

--- tests/Requests/ConnectedApps.php/ListConnectedAppsTest.php
+++ tests/Requests/ConnectedApps.php/ListConnectedAppsTest.php
@@ -11,11 +11,15 @@ use InterWorks\Tableau\Tests\Fixtures\TableauFixture;
 use Saloon\Http\Faking\MockClient;
 
 describe('ListConnectedAppsTest', function () {
-    test('can list connected apps ', function () {
+    test('can list connected apps', function () {
         $tableau = new Tableau(AuthType::PAT);
 
+        $mockClient = new MockClient([
+            ListConnectedAppsRequest::class => new TableauFixture('connected-apps/list-connected-apps')
+        ]);
+
         $listConnectedAppsRequest = new ListConnectedAppsRequest();
-        $response = $tableau->send($listConnectedAppsRequest);
+        $response = $tableau->send($listConnectedAppsRequest, $mockClient);
         expect($response->status())->toBe(200);
     });
 
@@ -30,7 +34,7 @@ describe('ListConnectedAppsTest', function () {
 
         // Validate the collection
         expect($connectedAppsCollection)->toBeInstanceOf(ConnectedAppsCollection::class);
-        expect($connectedAppsCollection->all())->toHaveCount(44);
+        expect($connectedAppsCollection->all())->toHaveCount(46);
 
         // Get all apps
         $apps = $connectedAppsCollection->all();
@@ -38,7 +42,7 @@ describe('ListConnectedAppsTest', function () {
         // Validate first app (with secret)
         $firstApp = $apps->first();
         expect($firstApp)->toBeInstanceOf(ConnectedApp::class);
-        expect($firstApp->name)->toBe('test_app__1');
+        expect($firstApp->name)->toBe('test_app__2');
         expect($firstApp->enabled)->toBe(true);
         expect($firstApp->clientId)->toBeString();
         expect($firstApp->projectIds)->toHaveCount(0);
@@ -49,27 +53,28 @@ describe('ListConnectedAppsTest', function () {
         // Validate secret
         expect($firstApp->secrets->all()->first())->toBeInstanceOf(ConnectedAppSecret::class);
         $firstAppSecret = $firstApp->secrets->all()->first();
-        expect($firstAppSecret->id)->toBe('528d17d3-8667-3ceb-868c-72fabde49c3e');
+        expect($firstAppSecret->id)->toBe('0ed29c00-5e26-3118-ac3a-7674b82b7a15');
         expect($firstAppSecret->createdAt->format('Y-m-d H:i:s'))->toBe('2023-06-16 20:02:29');
 
         // Validate second app (no secret)
         $secondApp = $apps->get(1);
-        expect($secondApp->name)->toBe('test_app__2');
+        expect($secondApp->name)->toBe('test_app__3');
         expect($secondApp->enabled)->toBe(false);
-        expect($secondApp->clientId)->toBe('9e9f1da0-773d-3f45-9727-d344335daca4');
+        expect($secondApp->clientId)->toBe('e440af30-8d50-34ad-b1a7-90a816b3dd42');
         expect($secondApp->unrestrictedEmbedding)->toBe(true);
         expect($secondApp->domainSafelist)->toBeNull();
         expect($secondApp->secrets)->toBeNull();
 
         // Test collection filtering methods
         $enabledApps = $connectedAppsCollection->enabled();
-        expect($enabledApps)->toHaveCount(43);
-        expect($enabledApps->first()->name)->toBe('test_app__1');
+        expect($enabledApps)->toHaveCount(44);
+        expect($enabledApps->first()->name)->toBe('test_app__2');
 
         $disabledApps = $connectedAppsCollection->disabled();
-        expect($disabledApps)->toHaveCount(1);
+        expect($disabledApps)->toHaveCount(2);
         expect($disabledApps->pluck('name')->toArray())->toEqual([
-            'test_app__2',
+            'test_app__3',
+            'Hello'
         ]);
     });
 });
