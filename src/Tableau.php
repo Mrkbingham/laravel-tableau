3b1a628b999efb6119524f0a91e30c8d5c3ff00c
mbingham <mark.bingham@interworks.com>
2025-06-17 10:08:07 -0500

    renames request

--- src/Tableau.php
+++ src/Tableau.php
@@ -7,7 +7,7 @@ use InterWorks\Tableau\Data\Authentication\PATAuthentication;
 use InterWorks\Tableau\Data\Authentication\UsernameAuthentication;
 use InterWorks\Tableau\Data\Site;
 use InterWorks\Tableau\Enums\AuthType;
-use InterWorks\Tableau\Requests\Authentication\SignInRequest;
+use InterWorks\Tableau\Requests\Authentication\SignIn;
 use InterWorks\Tableau\Services\VersionService;
 use RuntimeException;
 use Saloon\Http\Auth\HeaderAuthenticator;
@@ -55,12 +55,12 @@ class Tableau extends Connector
     public function boot(PendingRequest $pendingRequest): void
     {
         // If we've already authenticated, or are authenticating, we can skip this
-        if ($this->getToken() || $pendingRequest->getRequest() instanceof SignInRequest) {
+        if ($this->getToken() || $pendingRequest->getRequest() instanceof SignIn) {
             return;
         }
 
-        // Authenticate the request by sending a SignInRequest.
-        $signInResponse = $this->send(new SignInRequest($this->getAuth()))->dto();
+        // Authenticate the request by sending a SignIn.
+        $signInResponse = $this->send(new SignIn($this->getAuth()))->dto();
         $this->site = new Site($signInResponse->siteContentUrl, $signInResponse->siteId);
         $this->token = $signInResponse->token;
 
