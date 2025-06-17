3920bfd4ce5290a6cfd69c09d2e0ab11e9564b00
mbingham <mark.bingham@interworks.com>
2025-06-17 10:04:28 -0500

    renames to signout

--- src/Requests/Authentication/SignOut.php
+++ src/Requests/Authentication/SignOut.php
@@ -5,15 +5,19 @@ namespace InterWorks\Tableau\Requests\Authentication;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
-class SignOutRequest extends Request
+class SignOut extends Request
 {
     /**
      * The HTTP method of the request
+     *
+     *  @var Method
      */
     protected Method $method = Method::POST;
 
     /**
      * The endpoint for the request
+     *
+     *  @return string
      */
     public function resolveEndpoint(): string
     {
